7790158c88b93e73becac9d6be931fa150221350
Daphne Cornelisse <cor.daphne@gmail.com>
2024-08-27 18:24:34 -0400

    Add scene selector return info

--- pygpudrive/env/scene_selector.py
+++ pygpudrive/env/scene_selector.py
@@ -6,17 +6,19 @@ from pygpudrive.env.config import SelectionDiscipline
 
 
 def select_scenes(config):
-    """Selects a number of traffic scenes from the data directory based on the specified discipline.
+    """Selects a number of traffic scenes from the data directory based on
+    the specified discipline.
 
     Args:
-        config: Data configuration object containing the path to the data directory and the selection discipline.
+        config: Data configuration object containing the path to the data
+        directory and the selection discipline.
 
     Raises:
         ValueError: If the data directory does not exist or is empty.
         ValueError: If the data directory does not contain any traffic scenes.
 
     Returns:
-        _type_: _description_
+        list: A list of paths to the selected traffic scenes.
     """
     assert os.path.exists(config.path) and os.listdir(
         config.path
@@ -26,7 +28,8 @@ def select_scenes(config):
     selected_scenes = None
     if not any(scene.startswith("tfrecord") for scene in all_scenes):
         raise ValueError(
-            "The data directory does not contain any traffic scenes. Maybe you specified a path to the wrong folder?"
+            "The data directory does not contain any traffic scenes."
+            "Maybe you specified a path to the wrong folder?"
         )
 
     def random_sample(k):
@@ -58,14 +61,18 @@ def select_scenes(config):
 
     if not any(scene.startswith("tfrecord") for scene in selected_scenes):
         raise ValueError(
-            "The selected scene is not a traffic scenario. Something went wrong with the scene selection. Please check your data path."
+            "The selected scene is not a traffic scenario."
+            "Please check your data path."
         )
 
     scene_paths = [
         os.path.join(os.path.abspath(config.path), selected_scene)
         for selected_scene in selected_scenes
-    ] 
-    
-    print(f'\n--- Ratio unique scenes / number of worls = {len(np.unique(scene_paths))} / {len(scene_paths)} ---\n')
-        
+    ]
+
+    print(
+        f"\n--- Ratio unique scenes / number of worls = \
+        {len(np.unique(scene_paths))} / {len(scene_paths)} ---\n"
+    )
+
     return scene_paths
